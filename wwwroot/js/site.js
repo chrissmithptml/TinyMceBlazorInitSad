24595737f0c6c3da80983467f526c2171848641a
chris smith <chris.smith@ptml.com>
2022-10-14 09:59:58 +1300

    Example code of Blazor TinyMCE init issue

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.editorConfig = {
+    height: 500,
+    toolbar: 'undo redo | bold italic',
+    menubar: false,
+    init_instance_callback: function (editor) {
+        editor.setContent('<b>testy testy</b>');
+    }
+};
